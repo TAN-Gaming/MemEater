2838e8b787a05520d8c6286ab41a5feedd31d0e6
Thamatip Chitpong <tangaming123456@outlook.com>
2025-08-04 00:37:42 +0700

    Change license to 0BSD

--- main.c
+++ main.c
@@ -1,15 +1,8 @@
 /*
- * MemEater - A simple memory stress testing tool.
+ * Author: Thamatip Chitpong, 2024-2025
  *
- * Written in 2024-2025 by Thamatip Chitpong <tangaming123456@outlook.com>
- *
- * To the extent possible under law, the author(s) have dedicated all copyright
- * and related and neighboring rights to this software to the public domain
- * worldwide. This software is distributed without any warranty.
- *
- * You should have received a copy of the CC0 Public Domain Dedication along
- * with this software. If not, see
- * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ * SPDX-License-Identifier: 0BSD
+ * License-Filename: LICENSE.txt
  */
 
 #include <stdint.h>
